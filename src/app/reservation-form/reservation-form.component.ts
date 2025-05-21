c67f939b2a1ad2c72235ca421b72c6eb2e719a86
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-21 16:45:26 +0400

    set up validations for my form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css'],
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
-  onSubmit() {}
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: new FormControl('', [Validators.required]),
+      checkOutDate: new FormControl('', [Validators.required]),
+      guestName: new FormControl('', [Validators.required]),
+      guestEmail: new FormControl('', [Validators.required, Validators.email]),
+      roomNumber: new FormControl('', [Validators.required]),
+    });
+  }
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('Form Submitted!', this.reservationForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
