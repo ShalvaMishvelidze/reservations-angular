4d33130dc6950e6d55be4ed98a0199729aa9c1a5
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-21 17:02:42 +0400

    added reservation service functions

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
-
-  constructor() { }
+  private reservations: Reservation[] = [];
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find((reservation) => reservation.id === id);
+  }
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+  deleteReservation(id: string): void {
+    const index = this.reservations.findIndex((res) => res.id === id);
+    if (index !== -1) {
+      this.reservations.splice(index, 1);
+    }
+  }
+  updateReservation(updatedReservation: Reservation): void {
+    const index = this.reservations.findIndex(
+      (res) => res.id === updatedReservation.id
+    );
+    if (index !== -1) {
+      this.reservations[index] = updatedReservation;
+    }
+  }
 }
